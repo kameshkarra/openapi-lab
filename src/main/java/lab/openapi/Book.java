c4a5c552bb99c868ad49bed6cd63564d9602b4b8
kameshkarra <kamesh.karra@live.com>
2023-02-28 22:30:39 -0500

    Add Book application

---
+++ src/main/java/lab/openapi/Book.java
@@ -0,0 +1,28 @@
+package lab.openapi;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
